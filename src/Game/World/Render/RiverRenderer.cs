fab8728d9017aaf52c704510ac535f3bc2c1a122
Jörg Rosenauer <joergrosenauer@icloud.com>
2022-10-26 14:37:42 +0200

    river #2

--- src/Game/World/Render/RiverRenderer.cs
+++ src/Game/World/Render/RiverRenderer.cs
@@ -20,9 +20,21 @@ public class RiverRenderer : TileRenderer
             return null;
         }
 
-        if (tile.River.Count > 2)
+        switch (tile.River.Count)
         {
-            return _renderDirections.Get(tile.River);
+            case > 2:
+                return _renderDirections.Get(tile.River);
+            case 1:
+                return tile.River[0] switch
+                {
+                    Direction.West => GetRandom(tile.Position, "c_w", 2),
+                    Direction.East => GetRandom(tile.Position, "c_e", 2),
+                    Direction.SouthEast => GetRandom(tile.Position, "c_se", 3),
+                    Direction.SouthWest => GetRandom(tile.Position, "c_sw", 3),
+                    Direction.NorthWest => GetRandom(tile.Position, "c_nw", 3),
+                    Direction.NorthEast => GetRandom(tile.Position, "c_ne", 3),
+                    _ => throw new ArgumentOutOfRangeException(nameof(tile.River), "Unmapped river tile")
+                };
         }
 
         if (tile.River.Contains(Direction.East))
@@ -31,57 +43,67 @@ public class RiverRenderer : TileRenderer
             {
                 return GetRandom(tile.Position, "e_se", 2);
             }
+
             if (tile.River.Contains(Direction.SouthWest))
             {
                 return GetRandom(tile.Position, "e_sw", 2);
             }
+
             if (tile.River.Contains(Direction.West))
             {
                 return GetRandom(tile.Position, "e_w", 3);
             }
-            if (tile.River.Contains(Direction.NorthEast))
-            {
-                return GetRandom(tile.Position, "ne_e", 3);
-            }
-            if (tile.River.Contains(Direction.NorthWest))
-            {
-                return GetRandom(tile.Position, "nw_e", 3);
-            }
-            return GetRandom(tile.Position, "c_e", 2);
         }
 
         if (tile.River.Contains(Direction.NorthEast))
         {
+            if (tile.River.Contains(Direction.East))
+            {
+                return GetRandom(tile.Position, "ne_e", 3);
+            }
+
             if (tile.River.Contains(Direction.SouthEast))
             {
                 return GetRandom(tile.Position, "ne_se", 3);
             }
+
             if (tile.River.Contains(Direction.SouthWest))
             {
                 return GetRandom(tile.Position, "ne_sw", 3);
             }
+
             if (tile.River.Contains(Direction.West))
             {
                 return GetRandom(tile.Position, "ne_w", 3);
             }
-            return GetRandom(tile.Position, "c_ne", 3);
         }
 
         if (tile.River.Contains(Direction.NorthWest))
         {
+            if (tile.River.Contains(Direction.East))
+            {
+                return GetRandom(tile.Position, "nw_e", 3);
+            }
+
+            if (tile.River.Contains(Direction.NorthEast))
+            {
+                return GetRandom(tile.Position, "nw_ne", 2);
+            }
+
             if (tile.River.Contains(Direction.SouthWest))
             {
                 return GetRandom(tile.Position, "nw_sw", 3);
             }
+
             if (tile.River.Contains(Direction.SouthEast))
             {
                 return GetRandom(tile.Position, "nw_se", 3);
             }
+
             if (tile.River.Contains(Direction.West))
             {
                 return GetRandom(tile.Position, "nw_w", 3);
             }
-            return GetRandom(tile.Position, "c_nw", 3);
         }
 
         if (tile.River.Contains(Direction.SouthEast))
@@ -90,11 +112,11 @@ public class RiverRenderer : TileRenderer
             {
                 return GetRandom(tile.Position, "se_sw", 2);
             }
+
             if (tile.River.Contains(Direction.West))
             {
                 return GetRandom(tile.Position, "se_w", 2);
             }
-            return GetRandom(tile.Position, "c_se", 3);
         }
 
         if (tile.River.Contains(Direction.SouthWest))
@@ -103,15 +125,18 @@ public class RiverRenderer : TileRenderer
             {
                 return GetRandom(tile.Position, "sw_w", 2);
             }
-            return GetRandom(tile.Position, "c_sw", 3);
         }
 
-        if (tile.River.Contains(Direction.West))
+        return tile.River[0] switch
         {
-            return GetRandom(tile.Position, "c_w", 2);
-        }
-
-        return null;
+            Direction.West => GetRandom(tile.Position, "c_w", 2),
+            Direction.East => GetRandom(tile.Position, "c_e", 2),
+            Direction.SouthEast => GetRandom(tile.Position, "c_se", 3),
+            Direction.SouthWest => GetRandom(tile.Position, "c_sw", 3),
+            Direction.NorthWest => GetRandom(tile.Position, "c_nw", 3),
+            Direction.NorthEast => GetRandom(tile.Position, "c_ne", 3),
+            _ => throw new ArgumentOutOfRangeException(nameof(tile.River), "Unmapped river tile")
+        };
     }
 
     private Sprite GetRandom(Hex position, string name, int count)
